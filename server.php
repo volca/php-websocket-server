1a25ba5b36475f0e4b9cc5d286a2c13228a1e41a
volca <volca@tom.com>
2017-02-15 15:44:04 +0800

    add

---
+++ server.php
@@ -0,0 +1,34 @@
+<?php
+
+function saveLog($msg) {
+    // DATE_ATOM - msg
+    $msgType = 3;
+    $logFile = "devices_" . date("Ymd") . ".log";
+    $msg = date(DATE_ATOM) . " - $msg\n";
+    error_log($msg, $msgType, $logFile);
+}
+
+$server = new swoole_websocket_server("0.0.0.0", 8000);
+
+$server->on('open', function (swoole_websocket_server $server, $request) {
+    echo "server: handshake success with fd{$request->fd}\n";
+});
+
+$server->on('message', function (swoole_websocket_server $server, $frame) {
+    $arr = explode("\r\n", $frame->data);
+    $meta = array_shift($arr);
+    array_shift($arr);
+    if (empty($meta)) {
+        $device = new stdClass();
+    } else {
+        $device = json_decode($meta);
+    }
+    $device->beacon_count = count($arr);
+    saveLog(json_encode($device));
+});
+
+$server->on('close', function ($ser, $fd) {
+    echo "client {$fd} closed\n";
+});
+
+$server->start();
