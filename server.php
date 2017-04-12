4d9dceedc1fb19b0000a59f4e3584b3aabafd7b0
volca <volca@tom.com>
2017-04-12 13:33:36 +0800

    add dependences

--- server.php
+++ server.php
@@ -8,6 +8,12 @@ function saveLog($msg) {
     error_log($msg, $msgType, $logFile);
 }
 
+function saveData($data) {
+    $msgType = 3;
+    $logFile = "data_" . date("Ymd") . ".log";
+    error_log($data, $msgType, $logFile);
+}
+
 $server = new swoole_websocket_server("0.0.0.0", 8000);
 
 $server->on('open', function (swoole_websocket_server $server, $request) {
@@ -15,6 +21,7 @@ $server->on('open', function (swoole_websocket_server $server, $request) {
 });
 
 $server->on('message', function (swoole_websocket_server $server, $frame) {
+    //saveData($frame->data);
     $arr = explode("\r\n", $frame->data);
     $meta = array_shift($arr);
     array_shift($arr);
